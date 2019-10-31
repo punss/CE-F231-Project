9663ad7e954e3741766fd63499551e2cc8512180
Hridik Punukollu <saihridik@gmail.com>
2019-10-31 14:16:34 +0530

    Made code more appealing

--- Example_4_13.m
+++ Example_4_13.m
@@ -3,33 +3,35 @@
 %Done by: M Harikesha, Nipun Tewari, Hridik Punukollu
 
 %Taking input for the problem parameters
-a= input('Enter the length of the side of square in meters: ');
-s= input('Enter the specific gravity of material: ');
-w= 1;
+sideLength= input('Enter the length of the side of square in meters: ');
+specificGravity= input('Enter the specific gravity of material: ');
 G= input('Enter the specific weight of water: ');
-g= 9.81;
+disp("Assuming length of the log to be l m as it is not specified.")
+l = 1;
+accelerationDueToGravity= 9.81;
 
 %Doing the required calculations to the obtained values.
-fprintf ('Let depth of immersion be "x" and length of the log be "l" \n');
-x= s*a;
-OB= x/2;
-OG= a/2;
-BG= OG-OB;
+fprintf ('Let depth of immersion be "immersedLength" and length of the log be "l" \n');
+immersedLength = specificGravity*sideLength;
+centerOfBuoyancy= immersedLength/2;
+centerOfGravity= sideLength/2;
+BG= centerOfGravity-centerOfBuoyancy;
 
 %Displaying the calculated values.
-fprintf ('The center of buoyancy is: %f m\n', OB); 
-fprintf ('The center of gravity is: %f m\n', OG); 
+fprintf ('The center of buoyancy is: %f m\n', centerOfBuoyancy); 
+fprintf ('The center of gravity is: %f m\n', centerOfGravity); 
 fprintf ('BG = %f m\n', BG);
 
 %Performing some more calculations
-I= (1/12)*a^3;
-V= a*x;
+momentOfInertia= (1/12)*l*sideLength^3;
+immersedVolume= sideLength*immersedLength*l;
 
 %Displaying results
-BM= I/V;
+BM= momentOfInertia/immersedVolume;
 fprintf ('BM= %f m\n\n', BM);
+
 GM= BM-BG;
 fprintf('GM= %f m\n\n', GM);
-T = 6.28*(((a^2/12)/(g*GM))^0.5);
 
+T = 6.28*(((sideLength^2/12)/(accelerationDueToGravity*GM))^0.5);
 fprintf('The time period of rolling is given as: %f s \n\n', T);
